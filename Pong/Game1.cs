7c75222158186bd8671ab1fd7b66dfc79e7de26f
brian054 <brian.rowell54@gmail.com>
2025-02-20 21:07:53 -0500

    setup stuff yeeeee

--- Pong/Game1.cs
+++ Pong/Game1.cs
@@ -1,25 +1,26 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Numerics;
 
 //test commit 
 namespace Pong
 {
     public class Game1 : Game
     {
-        Texture2D ballTexture;
-        Vector2 ballPos;
-        float ballSpeed;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
 
-        Vector2 PaddleXPos;
-        float paddleSpeed;
-
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private PlayerPaddle player;
+        public Texture2D emptyTexture;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -28,72 +29,34 @@ namespace Pong
         // Query any required services and load any non-graphic related content
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            ballPos = new Vector2(_graphics.PreferredBackBufferWidth / 2,
-                _graphics.PreferredBackBufferHeight / 2);
-            ballSpeed = 100f;
+            // Set Window Size
+            graphics.PreferredBackBufferWidth = 1200;
+            graphics.PreferredBackBufferHeight = 800;
+            graphics.ApplyChanges(); // Apply the changes to the window size
 
-            PaddleXPos = new Vector2(50,
-                200);
-
+            this.IsFixedTimeStep = true;
+            this.TargetElapsedTime = TimeSpan.FromSeconds(1.0 / 60.0); // 60 updates per second
             base.Initialize();
         }
 
         // Called once per game, within the Initialize method, before the main game loop starts
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            ballTexture = Content.Load<Texture2D>("ball");
+            emptyTexture = new Texture2D(GraphicsDevice, 1, 1);
+            emptyTexture.SetData(new[] { Color.White });
+
+            player = new PlayerPaddle(emptyTexture, new Rectangle(GraphicsDevice.Viewport.Width / 2 - 50, GraphicsDevice.Viewport.Height / 2 - 50, 50, 50));
         }
 
-        // check collisions, gather input, play audio, etc.
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-            var keyState = Keyboard.GetState();
-
-            if (keyState.IsKeyDown(Keys.Up))
-            {
-                ballPos.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-
-            if (keyState.IsKeyDown(Keys.Down))
-            {
-                ballPos.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-
-            if (keyState.IsKeyDown(Keys.Left))
-            {
-                ballPos.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-
-            if (keyState.IsKeyDown(Keys.Right))
-            {
-                ballPos.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-
-            if (ballPos.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
-            {
-                ballPos.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
-            }
-            else if (ballPos.X < ballTexture.Width / 2)
-            {
-                ballPos.X = ballTexture.Width / 2;
-            }
-
-            if (ballPos.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
-            {
-                ballPos.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
-            }
-            else if (ballPos.Y < ballTexture.Height / 2)
-            {
-                ballPos.Y = ballTexture.Height / 2;
-            }
+          
 
             base.Update(gameTime);
         }
@@ -102,15 +65,9 @@ namespace Pong
         {
             GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(ballTexture, ballPos, 
-                null, Color.White, 0f, new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
-                Vector2.One * 3, SpriteEffects.None, 0f);
-            _spriteBatch.Draw(ballTexture, new Vector2(10, 200),
-                null, Color.White, 0f, new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
-                Vector2.One * 3, SpriteEffects.None, 0f);
-            _spriteBatch.End();
+            spriteBatch.Begin();
+            player.Draw(spriteBatch);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Pong/PlayerPaddle.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong {
+    internal class PlayerPaddle {
+
+        public Rectangle playerRect;
+        public Vector2 playerPos;
+        private int playerSpeed = 350; // pixels per second
+        private Texture2D playerTexture;
+
+        public PlayerPaddle(Texture2D emptyTexture, Rectangle rect) {
+            playerRect = rect;
+            playerPos = new Vector2(rect.X, rect.Y);
+            playerTexture = emptyTexture;
+
+        }
+
+        public void Update(KeyboardState kb, float dt) {
+            Vector2 movement = Vector2.Zero;
+            if (kb.IsKeyDown(Keys.W)) movement.Y -= 1; // Up
+            if (kb.IsKeyDown(Keys.S)) movement.Y += 1; // Down
+            if (kb.IsKeyDown(Keys.A)) movement.X -= 1; // Left
+            if (kb.IsKeyDown(Keys.D)) movement.X += 1; // Right
+
+            // normalize for diagonal movement
+            if (movement.LengthSquared() > 1) {
+                movement.Normalize();
+            }
+
+            // update pos
+            playerPos += movement * playerSpeed * dt;
+
+            // update rect pos - for collision ig
+            playerRect.X = (int) playerPos.X;
+            playerRect.Y = (int) playerPos.Y;
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Draw(playerTexture, playerRect, Color.Red);
+        }
+    }
+}
