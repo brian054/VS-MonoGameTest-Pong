115e3056051205d6458ade20f4e120ad405bc432
brian054 <brian.rowell54@gmail.com>
2025-03-13 21:19:25 -0400

    i just need to build this habit, ball is basically Paddle.cs they'll just have different Update methods....hmm again I don't really care to abstract it out that much it's such a small game. Then again tho....could just have GameEntity and in a Pong or BreakoutGame class have the GameEntity's you need right there.

--- Pong/Ball.cs
+++ Pong/Ball.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +8,24 @@ using System.Threading.Tasks;
 
 namespace Pong {
     internal class Ball {
+
+        private Vector2 ballPos;
+        private Rectangle ballRect; // a mf square
+        private int ballSpeed = 100; // pixels per second
+        private Texture2D dummyTexture;
+
+        public Ball(Rectangle rect, Texture2D dummyTexture) {
+            this.ballRect = rect;
+            this.ballPos = new Vector2(rect.X, rect.Y);
+            this.dummyTexture = dummyTexture;
+        }
+
+        public void Update() {
+
+        }
+        public void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Draw(dummyTexture, ballRect, Color.Red);
+        }
+
     }
 }
